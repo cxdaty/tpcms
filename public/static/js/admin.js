f7ec1033477aaeda7e467d85c96a6a2c06ca05ca
cxdaty <279618388@qq.com>
2018-06-15 18:05:32 +0800

    two

---
+++ public/static/js/admin.js
@@ -0,0 +1,196 @@
+/**
+ * 后台JS主入口
+ */
+
+var layer = layui.layer,
+    element = layui.element(),
+    laydate = layui.laydate,
+    form = layui.form();
+
+/**
+ * AJAX全局设置
+ */
+$.ajaxSetup({
+    type: "post",
+    dataType: "json"
+});
+
+/**
+ * 后台侧边菜单选中状态
+ */
+$('.layui-nav-item').find('a').removeClass('layui-this');
+$('.layui-nav-tree').find('a[href*="' + GV.current_controller + '"]').parent().addClass('layui-this').parents('.layui-nav-item').addClass('layui-nav-itemed');
+
+/**
+ * 通用单图上传
+ */
+layui.upload({
+    url: "/index.php/api/upload/upload",
+    type: 'image',
+    ext: 'jpg|png|gif|bmp',
+    success: function (data) {
+        if (data.error === 0) {
+            document.getElementById('thumb').value = data.url;
+        } else {
+            layer.msg(data.message);
+        }
+    }
+});
+
+/**
+ * 通用日期时间选择
+ */
+$('.datetime').on('click', function () {
+    laydate({
+        elem: this,
+        istime: true,
+        format: 'YYYY-MM-DD hh:mm:ss'
+    })
+});
+
+/**
+ * 通用表单提交(AJAX方式)
+ */
+form.on('submit(*)', function (data) {
+    $.ajax({
+        url: data.form.action,
+        type: data.form.method,
+        data: $(data.form).serialize(),
+        success: function (info) {
+            if (info.code === 1) {
+                setTimeout(function () {
+                    location.href = info.url;
+                }, 1000);
+            }
+            layer.msg(info.msg);
+        }
+    });
+
+    return false;
+});
+
+/**
+ * 通用批量处理（审核、取消审核、删除）
+ */
+$('.ajax-action').on('click', function () {
+    var _action = $(this).data('action');
+    layer.open({
+        shade: false,
+        content: '确定执行此操作？',
+        btn: ['确定', '取消'],
+        yes: function (index) {
+            $.ajax({
+                url: _action,
+                data: $('.ajax-form').serialize(),
+                success: function (info) {
+                    if (info.code === 1) {
+                        setTimeout(function () {
+                            location.href = info.url;
+                        }, 1000);
+                    }
+                    layer.msg(info.msg);
+                }
+            });
+            layer.close(index);
+        }
+    });
+
+    return false;
+});
+
+/**
+ * 通用全选
+ */
+$('.check-all').on('click', function () {
+    $(this).parents('table').find('input[type="checkbox"]').prop('checked', $(this).prop('checked'));
+});
+
+/**
+ * 通用删除
+ */
+$('.ajax-delete').on('click', function () {
+    var _href = $(this).attr('href');
+    layer.open({
+        shade: false,
+        content: '确定删除？',
+        btn: ['确定', '取消'],
+        yes: function (index) {
+            $.ajax({
+                url: _href,
+                type: "get",
+                success: function (info) {
+                    if (info.code === 1) {
+                        setTimeout(function () {
+                            location.href = info.url;
+                        }, 1000);
+                    }
+                    layer.msg(info.msg);
+                }
+            });
+            layer.close(index);
+        }
+    });
+
+    return false;
+});
+
+/**
+ * 清除缓存
+ */
+$('#clear-cache').on('click', function () {
+    var _url = $(this).data('url');
+    if (_url !== 'undefined') {
+        $.ajax({
+            url: _url,
+            success: function (data) {
+                if (data.code === 1) {
+                    setTimeout(function () {
+                        location.href = location.pathname;
+                    }, 1000);
+                }
+                layer.msg(data.msg);
+            }
+        });
+    }
+
+    return false;
+});
+
+
+/*弹出层*/
+/*
+ 参数解释：
+ title   标题
+ url     请求的url
+ id      需要操作的数据id
+ w       弹出层宽度（缺省调默认值）
+ h       弹出层高度（缺省调默认值）
+ */
+function admin_show(title, url, w, h) {
+    if (title == null || title == '') {
+        title = false;
+    }
+    ;
+    if (url == null || url == '') {
+        url = "404.html";
+    }
+    ;
+    if (w == null || w == '') {
+        w = ($(window).width() * 0.9);
+    }
+    ;
+    if (h == null || h == '') {
+        h = ($(window).height() - 50);
+    }
+    ;
+    layer.open({
+        type: 2,
+        area: [w + 'px', h + 'px'],
+        fix: false, //不固定
+        maxmin: true,
+        shadeClose: true,
+        shade: 0.4,
+        title: title,
+        content: url
+    });
+}
