8d8c1ec665126e510a369e082dd80b961b58c0e7
Administrator <you@example.com>
2018-06-28 17:58:08 +0800

    add data file

--- application/common.php
+++ application/common.php
@@ -237,24 +237,11 @@ function check_mobile_number($mobile)
     return preg_match($reg, $mobile) ? true : false;
 }
 
-function format_bytes($number)  
-{  
-    $len = strlen($number);  
-    if($len < 4)  
-    {  
-        return sprintf("%d b", $number);  
-    }  
-    if($len >= 4 && $len <=6)  
-    {  
-        return sprintf("%0.2f Kb", $number/1024);  
-    }  
-    if($len >= 7 && $len <=9)  
-    {  
-        return sprintf("%0.2f Mb", $number/1024/1024);  
-    }  
-      
-    return sprintf("%0.2f Gb", $number/1024/1024/1024);  
-} 
+function format_bytes($size, $delimiter = '') {
+    $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+    for ($i = 0; $size >= 1024 && $i < 5; $i++) $size /= 1024;
+    return round($size, 2) . $delimiter . $units[$i];
+}
 
 /**
  * 导出数据为excal文件
